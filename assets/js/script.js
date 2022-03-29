b5ce1a6c698e69ea407e5520ba707fad391ed294
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-29 11:55:05 +0530

    js code for paginated list in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,62 @@
-$(document).ready(function(){
-    $(".hamburger").click(function () {
-        $(".menus").toggleClass("headermenus-show");
-        $(".menus").toggleClass("headermenus");
-        $(this).toggleClass("cross");
-    });
+$(document).ready(function () {
+  $(".hamburger").click(function () {
+    $(".menus").toggleClass("headermenus-show");
+    $(".menus").toggleClass("headermenus");
+    $(this).toggleClass("cross");
+  });
 
-    let api_key ="8d6f976a3d568729504eb85502e74226";
-    $(".slider").html("");
+  let api_key = "8d6f976a3d568729504eb85502e74226";
+  $(".slider").html("");
 
-    // function calls to display movies according to categories 
-    if($(".movie-category").length > 0){
-    displayMovies("top_rated","top-rated-movies");
-    displayMovies("popular","most-popular-movies");
-    displayMovies("now_playing","now-playing-movies");
-    displayMovies("upcoming","upcoming-movies");
-    }
+  // function calls to display movies according to categories 
+  if ($(".movie-category").length > 0) {
+    displayMovies("top_rated", "top-rated-movies");
+    displayMovies("popular", "most-popular-movies");
+    displayMovies("now_playing", "now-playing-movies");
+    displayMovies("upcoming", "upcoming-movies");
+  }
 
-     // function calls to display tv shows according to categories 
-    if($(".tvshow-category").length > 0) {
-      displayTvshows("on_the_air","on-air");
-      displayTvshows("airing_today","airing-today");
-    }
+  // function calls to display tv shows according to categories 
+  if ($(".tvshow-category").length > 0) {
+    displayTvshows("on_the_air", "on-air");
+    displayTvshows("airing_today", "airing-today");
+  }
 
-// initialising variables for login form 
-    let loginform = $('.loginform'),
+  // initialising variables for login form 
+  let loginform = $('.loginform'),
     username = $('.username'),
     password = $('.password'),
     usererror = $('.usernameerror'),
     passerror = $('.passworderror');
-localStorage.setItem('username1', 'saurabh96');
-localStorage.setItem('password1', '123456');
-console.log(username);
+  localStorage.setItem('username1', 'saurabh96');
+  localStorage.setItem('password1', '123456');
+  console.log(username);
 
-//blur validation
-username.focusout(() => {
+  //blur validation
+  username.focusout(() => {
     usererror.html("");
     passerror.html("");
     $('.username').removeClass('errorbox');
     $('.password').removeClass('errorbox');
     if (username.val().length === 0) {
-        usererror.html("please enter username");
-        $('.username').addClass("errorbox");
+      usererror.html("please enter username");
+      $('.username').addClass("errorbox");
     }
-});
+  });
 
-password.focusout(() => {
+  password.focusout(() => {
     usererror.html("");
     passerror.html("");
     $('.username').removeClass('errorbox');
     $('.password').removeClass('errorbox');
     if (password.val().length === 0) {
-        passerror.html("please enter password");
-        $('.password').addClass("errorbox");
+      passerror.html("please enter password");
+      $('.password').addClass("errorbox");
     }
-})
+  })
 
-//login validation
-loginform.submit((event) => {
+  //login validation
+  loginform.submit((event) => {
     $('.username').removeClass('errorbox');
     $('.password').removeClass('errorbox');
     event.preventDefault();
@@ -64,33 +64,32 @@ loginform.submit((event) => {
     usererror.html("");
     passerror.html("");
     let user1 = localStorage.getItem('username1'),
-        pass1 = localStorage.getItem('password1');
+      pass1 = localStorage.getItem('password1');
     event.preventDefault();
 
     if (username.val().length === 0) {
-        usererror.html("please enter username");
-        $('.username').addClass("errorbox");
+      usererror.html("please enter username");
+      $('.username').addClass("errorbox");
     } else if (password.val().length === 0) {
-        passerror.html("please enter password")
-        $('.password').addClass("errorbox");
+      passerror.html("please enter password")
+      $('.password').addClass("errorbox");
     } else if (username.val() === user1 && password.val() === pass1) {
-        localStorage.setItem('validuser', 'true');
-        window.open('homepage.html');
+      localStorage.setItem('validuser', 'true');
+      window.open('homepage.html');
     } else {
-        passerror.html("invalid username or password");
+      passerror.html("invalid username or password");
     }
-})
+  })
 
-// slick slider code 
+  // slick slider code 
 
-$('.slider').slick({
+  $('.slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -113,23 +112,33 @@ $('.slider').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 })
 
+// code for paginated list page 
+
+let prev = $(".prev"),
+  current = $(".curent"),
+  next = $(".next"),
+  current_page = 1,
+  next_page = 2,
+  prev_page = 3,
+  total_pages = 100;
+  $(".view-more").click(()=>{
+    console.log(this.classname);
+  });
+
 // function to display movies according to categories
-function displayMovies(category,classname){
-  let api_key ="8d6f976a3d568729504eb85502e74226";
-    $.ajax({
-      url :"https://api.themoviedb.org/3/movie/"+category+"?api_key="+api_key+"&language=en-US&page=1",
-      type :"GET",
-      success :(data)=>{
-       let result = data.results;
-        result.forEach(i=>{
-          $("."+classname+" .slider").slick('slickAdd',`
+function displayMovies(category, classname) {
+  let api_key = "8d6f976a3d568729504eb85502e74226";
+  $.ajax({
+    url: "https://api.themoviedb.org/3/movie/" + category + "?api_key=" + api_key + "&language=en-US&page=1",
+    type: "GET",
+    success: (data) => {
+      let result = data.results;
+      result.forEach(i => {
+        $("." + classname + " .slider").slick('slickAdd', `
           <div class="movie-content ">
           <figure>
               <img src="https://image.tmdb.org/t/p/w500/${i.backdrop_path}" alt="Movie">
@@ -142,29 +151,29 @@ function displayMovies(category,classname){
           </div>
       </div>
           `);
-        })
+      })
 
-        $(".slider").slick();
-      },
-      error: (error)=>{
-        alert("something went wrong");
-        console.log(error);
-      }
+      $(".slider").slick();
+    },
+    error: (error) => {
+      alert("something went wrong");
+      console.log(error);
+    }
 
-    });
+  });
 
 }
 
 // function to tv shows according to categories
-function displayTvshows(category,classname){
-  let api_key ="8d6f976a3d568729504eb85502e74226";
-    $.ajax({
-      url :" https://api.themoviedb.org/3/tv/"+category+"?api_key="+api_key+"&language=en-US&page=1",
-      type :"GET",
-      success :(data)=>{
-       let result = data.results;
-        result.forEach(i=>{
-          $("."+classname+" .slider").slick('slickAdd',`
+function displayTvshows(category, classname) {
+  let api_key = "8d6f976a3d568729504eb85502e74226";
+  $.ajax({
+    url: " https://api.themoviedb.org/3/tv/" + category + "?api_key=" + api_key + "&language=en-US&page=1",
+    type: "GET",
+    success: (data) => {
+      let result = data.results;
+      result.forEach(i => {
+        $("." + classname + " .slider").slick('slickAdd', `
           <div class="movie-content ">
           <figure>
               <img src="https://image.tmdb.org/t/p/w500/${i.backdrop_path}" alt="Movie">
@@ -177,34 +186,15 @@ function displayTvshows(category,classname){
           </div>
       </div>
           `);
-        })
+      })
 
-        $(".slider").slick();
-      },
-      error: (error)=>{
-        alert("something went wrong");
-        console.log(error);
-      }
+      $(".slider").slick();
+    },
+    error: (error) => {
+      alert("something went wrong");
+      console.log(error);
+    }
 
-    });
+  });
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
