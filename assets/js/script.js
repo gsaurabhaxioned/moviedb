2678859dac919a42388dfb4d6196dd88238b5b61
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-01 16:32:50 +0530

    js code added for login validtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,13 +74,11 @@ $(document).ready(function () {
     $('.username').removeClass('errorbox');
     $('.password').removeClass('errorbox');
     event.preventDefault();
-
     usererror.html("");
     passerror.html("");
     let user1 = localStorage.getItem('username1'),
       pass1 = localStorage.getItem('password1');
     event.preventDefault();
-
     if (username.val().length === 0) {
       usererror.html("please enter username");
       $('.username').addClass("errorbox");
@@ -88,8 +86,7 @@ $(document).ready(function () {
     if (password.val().length === 0) {
       passerror.html("please enter password")
       $('.password').addClass("errorbox");
-    }
-    if (username.val() === user1 && password.val() === pass1) {
+    } else if (username.val() === user1 && password.val() === pass1) {
       localStorage.setItem('validuser', true);
       window.location.href = 'homepage.html';
     } else {
